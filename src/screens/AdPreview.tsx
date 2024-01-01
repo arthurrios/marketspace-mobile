f142edce3d3d4596d06ce3c1c691869689ab9641
arthur <arthur.rios007@gmail.com>
2024-01-01 14:03:46 -0300

    done ad preview screen

--- src/screens/AdPreview.tsx
+++ src/screens/AdPreview.tsx
@@ -16,6 +16,7 @@ import {
   ArrowLeft,
   PencilSimpleLine,
   Power,
+  Tag,
   TrashSimple,
 } from 'phosphor-react-native'
 import { useState } from 'react'
@@ -45,113 +46,137 @@ export function AdPreview() {
   }
 
   return (
-    <VStack flex={1} bgColor="$gray600" pt="$16">
-      <HStack px="$6" pb="$3" justifyContent="space-between">
-        <Pressable onPress={handleGoBack}>
-          <ArrowLeft />
-        </Pressable>
-        <Pressable>
-          <PencilSimpleLine />
-        </Pressable>
-      </HStack>
-      <Carousel images={images} adIsActive={adIsActive} />
-      <ScrollView px="$6" py="$5" flex={1}>
-        <VStack gap="$6" pb="$2">
-          <HStack gap="$2" alignItems="center">
-            <UserImage
-              height={24}
-              width={24}
-              uri="https://github.com/arthurrios.png"
-            />
-            <Text>Arthur Rios</Text>
-          </HStack>
-          <VStack gap="$2">
-            <HStack>
-              <View
-                bgColor="$gray500"
-                px="$2"
-                rounded="$full"
-                alignItems="center"
-              >
-                <Text
-                  fontSize="$2xs"
-                  fontFamily="$heading"
-                  color="$gray200"
-                  textTransform="uppercase"
-                >
-                  {isNew ? 'new' : 'used'}
-                </Text>
-              </View>
-            </HStack>
-            <HStack justifyContent="space-between" alignItems="baseline">
-              <Text fontFamily="$heading" fontSize="$xl" gap="$3" flex={1}>
-                Sneakers
-              </Text>
-              <HStack gap={6} alignItems="flex-end">
-                <Text
-                  mb={-1}
-                  fontSize="$sm"
-                  fontFamily="$heading"
-                  color="$blueLight"
-                >
-                  $
-                </Text>
-                <Text fontSize="$xl" fontFamily="$heading" color="$blueLight">
-                  26,30
-                </Text>
-              </HStack>
-            </HStack>
-            <Text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In sequi
-              ullam iste fugiat omnis itaque debitis! Velit, voluptatibus
-              perspiciatis. Facilis dolor fugiat error rem optio saepe maiores
-              id architecto ipsum.
-            </Text>
-          </VStack>
-          <VStack gap="$4">
-            <HStack gap="$2">
-              <Text fontFamily="$heading">Accept Trade?</Text>
-              <Text>{acceptTrade ? 'Yes' : 'No'}</Text>
+    <>
+      <VStack bgColor="$blueLight" pt="$16" px="$6" pb="$4" alignItems="center">
+        <Text fontFamily="$heading" color="$gray700">
+          Ad preview
+        </Text>
+        <Text fontFamily="$body" color="$gray700">
+          This is how your product will be displayed!
+        </Text>
+      </VStack>
+      <VStack flex={1} bgColor="$gray600">
+        <Carousel images={images} adIsActive={adIsActive} />
+        <ScrollView px="$6" py="$5" flex={1}>
+          <VStack gap="$6" pb="$2">
+            <HStack gap="$2" alignItems="center">
+              <UserImage
+                height={24}
+                width={24}
+                uri="https://github.com/arthurrios.png"
+              />
+              <Text>Arthur Rios</Text>
             </HStack>
             <VStack gap="$2">
-              <Text fontFamily="$heading">Payment methods:</Text>
+              <HStack>
+                <View
+                  bgColor="$gray500"
+                  px="$2"
+                  rounded="$full"
+                  alignItems="center"
+                >
+                  <Text
+                    fontSize="$2xs"
+                    fontFamily="$heading"
+                    color="$gray200"
+                    textTransform="uppercase"
+                  >
+                    {isNew ? 'new' : 'used'}
+                  </Text>
+                </View>
+              </HStack>
+              <HStack justifyContent="space-between" alignItems="baseline">
+                <Text fontFamily="$heading" fontSize="$xl" gap="$3" flex={1}>
+                  Sneakers
+                </Text>
+                <HStack gap={6} alignItems="flex-end">
+                  <Text
+                    mb={-1}
+                    fontSize="$sm"
+                    fontFamily="$heading"
+                    color="$blueLight"
+                  >
+                    $
+                  </Text>
+                  <Text fontSize="$xl" fontFamily="$heading" color="$blueLight">
+                    26,30
+                  </Text>
+                </HStack>
+              </HStack>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                sequi ullam iste fugiat omnis itaque debitis! Velit,
+                voluptatibus perspiciatis. Facilis dolor fugiat error rem optio
+                saepe maiores id architecto ipsum.
+              </Text>
+            </VStack>
+            <VStack gap="$4">
+              <HStack gap="$2">
+                <Text fontFamily="$heading">Accept Trade?</Text>
+                <Text>{acceptTrade ? 'Yes' : 'No'}</Text>
+              </HStack>
+              <VStack gap="$2">
+                <Text fontFamily="$heading">Payment methods:</Text>
 
-              {paymentMethods.map((item, index) => {
-                return <PaymentTag key={index} type={item} />
-              })}
+                {paymentMethods.map((item, index) => {
+                  return <PaymentTag key={index} type={item} />
+                })}
+              </VStack>
+            </VStack>
+            <VStack gap="$2" mb={50} bgColor="$gray600">
+              {!adIsActive ? (
+                <Button
+                  title="Reactivate ad"
+                  childrenIcon={
+                    <Power
+                      color="#EDECEE"
+                      size={16}
+                      style={{ marginRight: 8 }}
+                    />
+                  }
+                />
+              ) : (
+                <Button
+                  variant="secondary"
+                  title="Disable ad"
+                  childrenIcon={
+                    <Power
+                      color="#EDECEE"
+                      size={16}
+                      style={{ marginRight: 8 }}
+                    />
+                  }
+                />
+              )}
+              <Button
+                variant="tertiary"
+                title="Delete ad"
+                childrenIcon={
+                  <TrashSimple
+                    color="#3E3A40"
+                    size={16}
+                    style={{ marginRight: 8 }}
+                  />
+                }
+              />
             </VStack>
           </VStack>
-          <VStack gap="$2" mb={50} bgColor="$gray600">
-            {!adIsActive ? (
-              <Button
-                title="Reactivate ad"
-                childrenIcon={
-                  <Power color="#EDECEE" size={16} style={{ marginRight: 8 }} />
-                }
-              />
-            ) : (
-              <Button
-                variant="secondary"
-                title="Disable ad"
-                childrenIcon={
-                  <Power color="#EDECEE" size={16} style={{ marginRight: 8 }} />
-                }
-              />
-            )}
-            <Button
-              variant="tertiary"
-              title="Delete ad"
-              childrenIcon={
-                <TrashSimple
-                  color="#3E3A40"
-                  size={16}
-                  style={{ marginRight: 8 }}
-                />
-              }
-            />
-          </VStack>
-        </VStack>
-      </ScrollView>
-    </VStack>
+        </ScrollView>
+      </VStack>
+      <HStack bgColor="$gray700" gap="$3" pt="$5" px="$6" pb={28}>
+        <Button
+          variant="tertiary"
+          title="Go back and edit"
+          childrenIcon={<ArrowLeft size={16} style={{ marginRight: 8 }} />}
+          onPress={handleGoBack}
+        />
+        <Button
+          title="Publish"
+          childrenIcon={
+            <Tag size={16} style={{ marginRight: 8 }} color="#EDECEE" />
+          }
+        />
+      </HStack>
+    </>
   )
 }
