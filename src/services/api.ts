a271daf3b86d28c1e55eeb02e02f2e5b51800eae
arthur <arthur.rios007@gmail.com>
2024-01-13 18:34:07 -0300

    refresh token: fetch new token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
+/* eslint-disable prefer-const */
 /* eslint-disable camelcase */
-import { storageAuthTokenGet } from '@storage/storageAuthToken'
+import {
+  storageAuthTokenGet,
+  storageAuthTokenSave,
+} from '@storage/storageAuthToken'
 import { AppError } from '@utils/AppError'
 import axios, { AxiosError, AxiosInstance } from 'axios'
 
@@ -18,8 +22,8 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 }) as APIInstanceProps
 
-const failedQueue: Array<PromiseType> = []
-const isRefreshing = false
+let failedQueue: Array<PromiseType> = []
+let isRefreshing = false
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
@@ -54,6 +58,29 @@ api.registerInterceptTokenManager = (signOut) => {
               })
             })
           }
+          isRefreshing = true
+
+          return new Promise(async (resolve, reject) => {
+            try {
+              const { data } = await api.post('/sessions/refresh-token', {
+                refresh_token,
+              })
+              await storageAuthTokenSave({
+                token: data.token,
+                refresh_token: data.refresh_token,
+              })
+            } catch (error: any) {
+              failedQueue.forEach((request) => {
+                request.onFailure(error)
+              })
+
+              signOut()
+              reject(error)
+            } finally {
+              isRefreshing = false
+              failedQueue = []
+            }
+          })
         }
         signOut()
       }
