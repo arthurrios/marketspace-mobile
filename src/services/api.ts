d2b15d521b8a004867464a42e51155f32aa1b13f
arthur <arthur.rios007@gmail.com>
2024-01-13 17:53:25 -0300

    create: intercep token manager and verify invalid token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,36 @@
 import { AppError } from '@utils/AppError'
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+
+type SignOut = () => void
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
+}
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
-})
+}) as APIInstanceProps
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message))
-    } else {
-      return Promise.reject(error)
-    }
-  },
-)
-
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expired' ||
+          requestError.response.data?.message === 'token.invalid'
+        )
+          signOut()
+      }
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message))
+      } else {
+        return Promise.reject(requestError)
+      }
+    },
+  )
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager)
+  }
+}
 export { api }
